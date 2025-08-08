03f5f10978d03c28447200e91f23adef805a4de0
alex-poinot <77551076+alex-poinot@users.noreply.github.com>
2025-08-08 10:07:03 +0200

    Optimiser la configuration pour SPA

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -4,11 +4,11 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '634d3680-46b5-48e4-bdae-b7c6ed6b218a', // Remplacez par votre Client ID Azure AD
     authority: 'https://login.microsoftonline.com/e1029da6-a2e7-449b-b816-9dd31f7c2d83', // Remplacez par votre Tenant ID
-    redirectUri: 'http://localhost:4200',
-    postLogoutRedirectUri: 'http://localhost:4200'
+    redirectUri: 'http://localhost:4200/',
+    postLogoutRedirectUri: 'http://localhost:4200/'
   },
   cache: {
-    cacheLocation: 'localStorage',
+    cacheLocation: 'sessionStorage',
     storeAuthStateInCookie: false
   },
   system: {
